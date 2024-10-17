4a7111f49b52bbc767348899c3f81a2d78ade90c
Christopher Attar <cattar@protonmail.ch>
2024-10-17 11:04:50 -0400

    Update Saved-Search-Syntax.sql

--- Saved-Search-Syntax.sql
+++ Saved-Search-Syntax.sql
@@ -2,3 +2,33 @@
 THEN '#FE9999' ELSE 'None' END||';'||{amount}||'</div>'
 
 
+/* #1. RETURN A TEXT FIELD */
+CASE 
+    WHEN {type} = 'Invoice' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}      
+    WHEN {type} = 'Sales Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}
+ELSE NULL  END
+
+CASE
+    WHEN {type} = 'Bill' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}      
+    WHEN {type} = 'Sales Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}
+ELSE NULL  END
+
+CASE
+    WHEN {type} = 'Purchase Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}      
+    WHEN {type} = 'Sales Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}
+ELSE NULL  END
+
+/* #2. RETURN AN AMOUNT */
+CASE
+    WHEN {type} = 'Bill' AND {custbody_fieldname} IS NOT NULL THEN {amount}      
+    WHEN {type} = 'Sales Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}
+
+ELSE NULL  END
+
+CASE WHEN(
+(CASE
+    WHEN {type} = 'Bill' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname}) 
+    =    
+    (CASE WHEN {type} = 'Sales Order' AND {custbody_fieldname} IS NOT NULL THEN {custbody_fieldname})
+) THEN {amount}
+ELSE NULL  END
