0b07e19d3c067f2d6d1cd49c25f43d82e99b02b6
Christopher Attar <cattar@protonmail.ch>
2025-03-17 09:38:53 -0400

    Update Saved-Search-Syntax.sql

--- Saved-Search-Syntax.sql
+++ Saved-Search-Syntax.sql
@@ -48,3 +48,37 @@ decode({type},'Sales Order',{applyingtransaction.amount})-decode({type},'Sales O
 (decode({type},'Sales Order',{applyingtransaction.rate}))-((decode({type},'Sales Order',{fulfillingtransaction.cogsamount},0))/{fulfillingtransaction.quantity})
 decode({type},'Sales Order',({applyingtransaction.amount}/{applyingtransaction.quantity}))
 decode({type},'Item Receipt',{trandate})
+
+
+/* SALES AMOUNTS */
+(
+(CASE WHEN {account} = 'GLSALES1' THEN {amount} ELSE 0 END) + 
+(CASE WHEN {account} = 'GSALES2' THEN {amount} ELSE 0 END) +
+(CASE WHEN {account} = 'GLSALES3' THEN {amount} ELSE 0 END) +
+(CASE WHEN {account} = 'GLSALES4' THEN {amount} ELSE 0 END)
+)
+
+ 
+/* COSTS OF SALES */
+(
+(CASE WHEN {account} = 'GLCOGS1' THEN {amount} ELSE 0 END) +
+(CASE WHEN {account} = 'GLCOGS2' THEN {amount} ELSE 0 END)
+)
+
+/* GM CALC */
+
+
+1 - (
+  (
+    (CASE WHEN {account} = 'GLCOGS01' THEN {amount} ELSE 0 END) +
+    (CASE WHEN {account} = 'GLCOGS02' THEN {amount} ELSE 0 END)
+  ) 
+  / 
+(NULLIF(
+    (CASE WHEN {account} = 'GLSALES01' THEN {amount} ELSE 0 END) + 
+    (CASE WHEN {account} = 'GLSALES02' THEN {amount} ELSE 0 END) +
+    (CASE WHEN {account} = 'GLSALES03' THEN {amount} ELSE 0 END) +
+    (CASE WHEN {account} = 'GLSALES04' THEN {amount} ELSE 0 END),
+    0
+  ))
+)
